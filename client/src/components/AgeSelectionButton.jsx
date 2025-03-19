a54ec2674c223a86259336d879960738d2818560
DylanCas16 <dylancastrillon@outlook.com>
2025-03-19 10:31:29 +0000

    Added AgeSelectionButton for the levels.

---
+++ client/src/components/AgeSelectionButton.jsx
@@ -0,0 +1,8 @@
+
+export default function AgeSelectionButton() {
+    return (
+        <div className={"w-28 h-12 flex items-center justify-center border-2 rounded-md border-red-500 bg-red-300 text-black "}>
+            6-7 years
+        </div>
+    )
+}
