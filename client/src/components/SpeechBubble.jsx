1c0b470f8f4a5382f296fcc6058d6c9d73d529da
TychoQS <tycho.quintana@gmail.com>
2025-03-19 01:31:58 +0000

    feat: SpeechBubble template implemented

---
+++ client/src/components/SpeechBubble.jsx
@@ -0,0 +1,70 @@
+/*
+======================== USAGE ====================
+Just call the component giving a text and a subject in this format:
+        <SpeechBubble
+            Text={"Text to display"}
+            Subject={"Related subject"}>
+        </SpeechBubble>
+
+======================== EXAMPLE ====================
+
+        <SpeechBubble
+            Text={"This is a very funny art game"}
+            Subject={"Art"}>
+        </SpeechBubble>
+
+ */
+
+export default function SpeechBubble(props) {
+    const Text = props.Text
+    const Subject = props.Subject
+
+    const getSpeechBubbleOvalBackgroundColor = (Subject) => {
+        switch (Subject) {
+            case "Math":
+                return "bg-[#1BA8E4]"
+            case "English":
+                return "bg-[#EFF66E]"
+            case "Geography":
+                return "bg-[#ED6EF6]"
+            case "Art":
+                return "bg-[#F67C6E]"
+            case "Science":
+                return "bg-[#Science]"
+            default:
+            return ""
+        }
+    }
+
+    const getSpeechBubbleSquareBackgroundColor = (Subject) => {
+        switch (Subject) {
+            case "Math":
+                return "border-[#1BA8E4]"
+            case "English":
+                return "border-[#EFF66E]"
+            case "Geography":
+                return "border-[#ED6EF6]"
+            case "Art":
+                return "border-[#F67C6E]"
+            case "Science":
+                return "border-[#Science]"
+            default:
+                return ""
+        }
+    }
+
+    return (
+        <section className={`relative mt-15 border-4 border-black rounded-[90px] mx-auto px-10 py-6 max-w-sm min-h-fit
+                            ${getSpeechBubbleOvalBackgroundColor(Subject)}`}>
+            <p className="text-center font-bold color text-black align-baseline">
+                {Text}
+            </p>
+            <section className={`absolute top+[45%] right-[0.1rem]
+                        border-l-[5vh] border-l-transparent
+                        border-r-[5vh] border-r-transparent
+                        border-t-[10vh] rotate-[-30deg] bg-bl
+                        ${getSpeechBubbleSquareBackgroundColor(Subject)}`} >
+            </section>
+        </section>
+    );
+}
