eb7636157f519964e0a776f01448a6379dd177c1
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2025-03-19 21:46:13 +0000

    edit: Animal icon style

--- client/src/components/AnimalIcon.jsx
+++ client/src/components/AnimalIcon.jsx
@@ -3,12 +3,16 @@ import React from "react";
 const AnimalIcon = ({ animal, imageURL, borderColor, style }) => {
     return (
         <div className="absolute" style={style}>
-            <img
-                src={imageURL}
-                alt={animal}
-                className="w-24 h-24 md:w-32 md:h-32 rounded-full border-8 bg-white"
+            <div
+                className="w-20 h-20 md:w-32 md:h-32 rounded-full border-8 bg-white overflow-hidden"
                 style={{ borderColor }}
-            />
+            >
+                <img
+                    src={imageURL}
+                    alt={animal}
+                    className="w-full h-full object-cover"
+                />
+            </div>
         </div>
     );
 };
