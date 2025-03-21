bd2c5355f195219df7e46bf4693cab4c18701350
DylanCas16 <dylancastrillon@outlook.com>
2025-03-21 17:09:39 +0000

    updated GameCard.jsx

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,14 +1,26 @@
+/*
+USAGE
+
+<GameCard
+      Title={"Game name"}
+      Description={"Game description Game description Game description Game description Game description"}
+      Completed={true}
+      Subject={"Art"}
+      Score={"0"}>
+</GameCard>
+*/
+
 export default function GameCard(props) {
     const { Title, Description, Completed, Subject, Score } = props;
 
     const getBackgroundStyle = () => {
         if (Completed) {
             const backgroundImages = {
-                "Art": "url(../images/art.png)",
-                "Science": "url(../images/science.png)",
-                "English": "url(../images/english.png)",
-                "Geography": "url(../images/geography.png)",
-                "Math": "url(../images/math.png)"
+                "Art": "url(../images/platypus.png)",
+                "Science": "url(../images/platypus.png)",
+                "English": "url(../images/platypus.png)",
+                "Geography": "url(../images/platypus.png)",
+                "Math": "url(../images/platypus.png)"
             };
 
             return {
@@ -16,7 +28,7 @@ export default function GameCard(props) {
                 backgroundSize: "contain",
                 backgroundRepeat: "no-repeat",
                 backgroundPosition: "center",
-                position: "relative"
+                position: "relative",
             };
         }
         return {};
@@ -24,15 +36,14 @@ export default function GameCard(props) {
 
     return (
         <section
-            className="w-80 h-96 flex flex-col m-auto items-center justify-evenly border-4 rounded-lg bg-orange-300 border-orange-500 text-black p-4 shadow-lg relative"
-            style={getBackgroundStyle()}
-        >
+            className="w-80 h-90 flex flex-col m-auto items-center justify-evenly border-4 rounded-lg bg-orange-300 border-orange-500 text-black p-4 shadow-lg relative"
+            style={getBackgroundStyle()}>
             <div className="text-2xl font-black text-center">{Title}</div>
             <div className="text-sm text-center font-medium p-2 rounded-md">{Description}</div>
             <button className="cursor-pointer w-40 h-12 border-b-8 rounded-full hover:bg-orange-400 hover:border-none bg-orange-500 border-b-violet-950 font-black text-xl shadow-md">
                 Start
             </button>
-            <div className="text-lg font-semibold">Previous Score: {Score}</div>
+            <div className="text-lg font-semibold">Previous Score: {Score ?? "N/A"}</div>
         </section>
     );
 }
