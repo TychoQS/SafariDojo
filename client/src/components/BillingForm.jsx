3de87191685b908d8c4137660190b7c9762cd885
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2025-03-24 22:12:49 +0000

    edit: change billing form style

--- client/src/components/BillingForm.jsx
+++ client/src/components/BillingForm.jsx
@@ -5,8 +5,9 @@ import Title from "@/components/Title";
 
 export default function BillingForm() {
     return (
-        <section className={"w-[50rem] h-[40rem] border-4 rounded-lg m-auto flex flex-col items-center " +
-            "justify-start bg-[#FFDEB6] border-[#FBAF00] p-6 mt-[1rem] mb-[-30rem]"}>
+        <section
+            className="border-4 rounded-lg m-auto flex flex-col items-center justify-start bg-[#FFDEB6] border-[#FBAF00]
+             mb-[-1vh] pt-[10px] pl-[50px] pr-[50px] pb-[30px]">
             <Title level={2} className="mb-4">Billing Information</Title>
             <Form id={"BillingForm"} className={"flex flex-col items-center text-[#372E55] mt-[1.5rem] w-full gap-4"}>
                 <Input size="large" id="CardholderName" label="Cardholder name"/>
