96bfbad958df4c308d66b15d1e2c5d02d9ed5b9a
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2025-03-24 13:22:45 +0000

    edit: modified PopularGameCard circle

--- client/src/components/PopularGameCard.jsx
+++ client/src/components/PopularGameCard.jsx
@@ -8,12 +8,12 @@ const Card = ({gameSubject, isCompleted, medalType}) => {
 
     return (
         <div
-            className="flex items-center w-150 h-15 bg-white border-3 border-[#3E3F4F] rounded-lg overflow-hidden shadow-lg">
+            className="relative flex items-center w-150 h-15 bg-white border-3 border-[#3E3F4F] rounded-lg overflow-hidden shadow-lg">
             <div className="flex items-center px-4 py-2 w-full">
                 <div className="flex flex-col justify-center items-center w-20">
-                    <span className={`text-[#3E3F4F] text-xs ${patrickHand.className}`}>
+                      <span className={`text-[#3E3F4F] text-xs ${patrickHand.className}`}>
                         {subjectName}
-                    </span>
+                      </span>
                     <div className="w-5 h-5 rounded-full" style={{backgroundColor: borderColor}}/>
                 </div>
 
@@ -37,10 +37,11 @@ const Card = ({gameSubject, isCompleted, medalType}) => {
             </div>
 
             <div
-                className={`w-10 h-10 flex justify-center items-center text-lg rounded-full ${isCompleted ? 'bg-green-500' : 'bg-gray-400'} mr-3`}>
+                className={`absolute right-0 -translate-x-1/2 top-1/2 transform -translate-y-1/2 w-10 h-10 flex justify-center items-center text-lg rounded-full ${isCompleted ? 'bg-green-500' : 'bg-gray-400'}`}>
                 {isCompleted ? '✓' : '♦'}
             </div>
         </div>
     );
 };
+
 export default Card;
