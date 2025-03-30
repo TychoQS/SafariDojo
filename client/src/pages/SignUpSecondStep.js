f4dc0e5cd2b9386dae6f13825b93f09927274ab6
TychoQS <tycho.quintana@gmail.com>
2025-03-30 21:02:51 +0100

    feat: Register functionality Added

--- client/src/pages/SignUpSecondStep.js
+++ client/src/pages/SignUpSecondStep.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, {useRef} from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import FormField from "@/components/FormField";
+import users from "../../../database/jsondata/Users.json";
+import logIn from "@/pages/LogIn";
+import {useAuth} from "@/pages/context/AuthContext";
+import {useRouter} from "next/router";
 
 const SignUpSecondStep = () => {
+    const { logIn } = useAuth();
+    const router = useRouter();
+    const hashPassword = async (password) => {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(password);
+        const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        return hashArray.map(byte => byte.toString(16).padStart(2, "0")).join("");
+    };
+
+    const handleSubmit = async (formData) => {
+        const PreviousData = JSON.parse(sessionStorage.getItem('signupData'));
+        sessionStorage.removeItem(PreviousData);
+        const {Password} = formData;
+        const HashedPassword = await hashPassword(Password);
+        const userData = {
+            ...PreviousData,
+            password: Password,
+            profilePhoto: "Sheep"
+        };
+        try {
+            const response = await fetch("http://localhost:8080/api/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userData),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                const token = `fakeTokenForUser-${Date.now()}`;
+                logIn(token, userData);
+                router.push("/");
+            } else {
+                console.error("Error en la respuesta:", data.message);
+            }
+        } catch (error) {
+            console.error("Error en la petición:", error);
+        }
+    };
+
     return (
         <div className="flex flex-col min-h-screen bg-cover bg-center m-0"
              style={{ backgroundImage: "url('/images/LogBackground.png')" }}>
@@ -37,9 +83,9 @@ const SignUpSecondStep = () => {
                     ]}
                     buttonText="Create"
                     buttonSize="small"
-                    buttonLink="/MyProfile"
                     linkText="Already have an account?"
                     linkUrl="/LogIn"
+                    onSubmit={handleSubmit}
                 />
             </main>
             <Footer />
