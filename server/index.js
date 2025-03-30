7453632581b4647205f78815f5f8aa5dd3b8dc6b
TychoQS <tycho.quintana@gmail.com>
2025-03-30 21:01:13 +0100

    add: Register endpoint and logic to register on json

--- server/index.js
+++ server/index.js
@@ -2,14 +2,43 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const cors = require('cors');
+const { readFile, writeFile } = require('fs');
+const {join} = require("node:path");
 
-// Cross Origin Resource Sharing
+
+
+const dbFilePath = join(__dirname, '../database/jsondata/Users.json');
+
 app.use(cors());
+app.use(express.json());
 
-// API Endpoint
 app.get("/api/home", (req, res) => {
     res.json({message: "Hello World!"});
 })
+
+app.post("/api/signup", (req, res) => {
+    try {
+        let userData = req.body;
+        const dbFilePath = join(__dirname, '../database/jsondata/Users.json');
+        readFile(dbFilePath, 'utf-8', (err, data) => {
+            if (err) {
+                console.error("Error al leer el archivo:", err);
+            }
+            const users = JSON.parse(data);
+            users.push(userData);
+            writeFile(dbFilePath, JSON.stringify(users, null, 2), (writeErr) => {
+                if (writeErr) {
+                    console.error("Error al escribir en el archivo:", writeErr);
+                    return res.status(500).json({ message: "Error al guardar el usuario" });
+                }
+                res.status(201).json({ message: "Usuario registrado con éxito", userData });
+            });
+        });
+    } catch (error) {
+        console.error("Error al registrar usuario:", error);
+        res.status(500).json({ message: "Error interno del servidor" });
+    }
+});
 
 
 app.listen(PORT, () => {
