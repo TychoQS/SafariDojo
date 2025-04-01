ed5ba84da0ba484e7e4d11926dfb834adddd3fd5
TychoQS <tycho.quintana@gmail.com>
2025-04-01 04:07:12 +0100

    edit: Using new endpoint

--- client/src/pages/SignUpSecondStep.js
+++ client/src/pages/SignUpSecondStep.js
@@ -29,7 +29,7 @@ const SignUpSecondStep = () => {
             profilePhoto: "Sheep"
         };
         try {
-            const response = await fetch("http://localhost:8080/api/signup", {
+            const response = await fetch("http://localhost:8080/api/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
