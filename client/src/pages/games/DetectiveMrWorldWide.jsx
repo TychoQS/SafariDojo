b4d19d88335fd5e60c757f9b87aced2aa338b147
Alejandro <deolozaga2004@gmail.com>
2025-04-29 23:12:39 +0100

    edit: changed game Mechanic, now 1/6 parts of the image are visible.

--- client/src/pages/games/DetectiveMrWorldWide.jsx
+++ client/src/pages/games/DetectiveMrWorldWide.jsx
@@ -16,97 +16,144 @@ function DetectiveMrWorldWide() {
     const [guess, setGuess] = useState("");
     const [message, setMessage] = useState("");
     const [item, setItem] = useState({Name: "", Image: ""});
-    const [next, setNext] = useState(false);
     const [score, setScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
     const [tries, setTries] = useState(4);
-    const [finished, setFinished] = useState(false);
     const lifesRef = useRef(null);
+    const [gameStatus, setGameStatus] = useState("playing");
+    const [visibleSection, setVisibleSection] = useState(0);
+
 
     useEffect(() => {
         setItem(getImage());
+        setVisibleSection(getRandomSection());
     }, []);
 
     const name = item.Name;
     const flag = item.Image;
 
+    function getRandomSection() {
+        return Math.floor(Math.random() * 6);
+    }
+
     function validatePicture(name, guess) {
-        if (tries === 0) {
-            setFinished(true);
+        if (tries <= 1) {
+            setGameStatus("finished")
             setBestScore(Math.max(bestScore, score));
             if (score < 300 && lifesRef.current) {
                 lifesRef.current.loseLife();
             }
+        } else if (tries > 0) {
+            setGameStatus("waiting");
         }
         if (name.trim().toLowerCase() === guess.trim().toLowerCase()) {
-            setMessage("Nice, you must be a real explorer!!!")
-            setScore(score + 100);
+            setMessage("Nice!")
+            setScore(prevScore => prevScore + 5);
         } else {
-            setMessage("Keep guessing!");
+            setMessage("Nope!");
         }
-        setNext(true);
+
     }
 
     function nextGame() {
         setItem(getImage());
+        setTries(prevTries => prevTries - 1);
         setGuess("");
         setMessage("");
-        setNext(false);
-        setTries(tries-1);
+        setGameStatus("playing")
+        setVisibleSection(getRandomSection());
     }
 
+    function resetGame() {
+        setTries(4);
+        setScore(0);
+        setGameStatus("playing");
+        setItem(getImage());
+        setGuess("");
+        setMessage("");
+        setVisibleSection(getRandomSection());
+    }
+
+    const getClipPathStyle = () => {
+        const cols = 3;
+        const rows = 2;
+
+        const col = visibleSection % cols;
+        const row = Math.floor(visibleSection / cols);
+
+        const xStart = (col / cols) * 100;
+        const xEnd = ((col + 1) / cols) * 100;
+        const yStart = (row / rows) * 100;
+        const yEnd = ((row + 1) / rows) * 100;
+
+        return {
+            clipPath: `inset(${yStart}% ${100-xEnd}% ${100-yEnd}% ${xStart}%)`,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover"
+        };
+    };
+
+
     return (
-        <div className="app min-h-screen flex flex-col bg-PS-main-purple ">
+        <div className="app min-h-screen flex flex-col bg-PS-main-purple">
             <Header></Header>
             <section className={"min-h-screen flex flex-col justify-evenly bg-PS-main-purple"}>
-
                 <div className="flex items-end justify-end">
                     <Lifes ref={lifesRef} />
                 </div>
+
 
                 <div className="h-150 w-175 flex flex-col self-center items-center justify-evenly border-4 rounded-2xl
             border-PS-dark-yellow bg-PS-light-yellow">
 
                     <div className="max-w-80 max-h-80 flex justify-center items-center border-black border-4">
-                        {flag ? <img src={flag} alt={name} className="max-w-full max-h-full object-contain"/> :
+                        {flag ? <img src={flag}
+                                     alt={name}
+                                     className="max-w-full max-h-full object-contain"
+                                     style={getClipPathStyle()}/> :
                             <p>No image available</p>}
                     </div>
 
                     {message && <p className={"text-black text-xl"}>{message}</p>}
 
-                    <input className={"h-10 w-70 border-2 rounded-lg bg-[#E8B1EC] border-[#ED6EF6] text-black text-xl"}
+
+                    <input className={"bg-[#E8B1EC] h-[50px] w-[350px] px-4 py-3 text-[20px] text-gray-600 outline-none rounded-lg border-2 transition-colors" +
+                        " duration-300 border-solid border-gray-500 focus:border-[black] focus:text-black"}
                            placeholder={"Introduce the flag name..."} value={guess}
                            onChange={(e) => setGuess(e.target.value)}/>
 
                     <div className="flex flex-row justify-center">
-                        {!next && !finished ?
-                            <button className={"cursor-pointer h-15 w-35 rounded-4xl border-b-8 hover:border-none " +
-                                "text-lg border-[#ED6EF6] bg-[#E8B1EC] text-black"}
+                        {gameStatus === "playing" ?
+                            <button className={"cursor-pointer h-15 w-35 rounded-2xl " +
+                                "text-lg border-[#ED6EF6] bg-[#E8B1EC] text-black duration-300 hover:scale-110 "}
                                     onClick={() => validatePicture(name, guess)}>
                                 Resolve
                             </button> : null}
 
-                        {next && !finished ?
+                        {gameStatus === "waiting" ?
                             <button className={"cursor-pointer h-15 w-35 rounded-4xl border-b-8 hover:border-none " +
                                 "text-lg border-[#ED6EF6] bg-[#E8B1EC] text-black"}
                                     onClick={() => nextGame()}>
                                 Next
                             </button> : null}
 
-                        {finished ? <button className={"cursor-pointer h-15 w-35 rounded-4xl border-b-8 hover:border-none " +
-                            "text-lg border-[#ED6EF6] bg-[#E8B1EC] text-black"}
-                                            onClick={() => nextGame() || setFinished(false) || setTries(4) || setScore(0)}>
-                            Retry
-                        </button> : null}
-
-                        {finished ?
-                            <Link href={{pathname: "../GameSelectionPage", query: {Points: bestScore, Subject: "Geography"}}}>
+                        {gameStatus === "finished" ?
+                            <div>
                                 <button className={"cursor-pointer h-15 w-35 rounded-4xl border-b-8 hover:border-none " +
-                                    "text-lg border-[#ED6EF6] bg-[#E8B1EC] text-black"}>
-                                    Finish game
+                                    "text-lg border-[#ED6EF6] bg-[#E8B1EC] text-black"}
+                                        onClick={() => resetGame()}>
+                                    Retry
                                 </button>
-                            </Link>
-                                : null}
+
+                                <Link href={{pathname: "../GameSelectionPage", query: {Points: bestScore, Subject: "Geography"}}}>
+                                    <button className={"cursor-pointer h-15 w-35 rounded-4xl border-b-8 hover:border-none " +
+                                        "text-lg border-[#ED6EF6] bg-[#E8B1EC] text-black"}>
+                                        Finish game
+                                    </button>
+                                </Link>
+                            </div> : null}
+
                     </div>
 
                     <div className={"text-black text-2xl font-black flex justify-between"}><p className="text-gray-600 font-light">Score: {score}</p></div>
