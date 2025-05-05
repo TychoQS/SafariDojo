032897b262df78e19c7a44ee2aa7a7612ff1c206
lauraheerrera <159728136+lauraheerrera@users.noreply.github.com>
2025-05-05 13:55:40 +0100

    now best score stores in DB

--- client/src/pages/games/LetterSoup.jsx
+++ client/src/pages/games/LetterSoup.jsx
@@ -180,8 +180,43 @@ export default function LetterSoup() {
         setCurrentWord("");
     };
 
+    const saveScore = () => {
+        try {
+            const previousURL = localStorage.getItem("previousURL");
+            if (previousURL) {
+                const url = new URL(previousURL);
+                const gameData = url.searchParams.get("Game");
+                const age = url.searchParams.get("Age");
+
+                if (gameData && age) {
+                    const key = `${gameData}_${age}_bestScore`;
+                    const storedScore = parseInt(localStorage.getItem(key) || "0", 10);
+
+                    if (points > storedScore) {
+                        localStorage.setItem(key, points.toString());
+                    }
+
+                    const typeMedal = age === "easy"
+                        ? "BronzeMedal"
+                        : age === "medium"
+                            ? "SilverMedal"
+                            : "GoldMedal";
+
+                    const medalKey = `${gameData}_${typeMedal}`;
+                    const medalStatus = localStorage.getItem(medalKey) === "1";
+                    if (!medalStatus) {
+                        localStorage.setItem(medalKey, "1");
+                    }
+                }
+            }
+        } catch (error) {
+            console.error("Error processing score or medal update:", error);
+        }
+    }
+
     const closeModal = () => {
         setShowModal(false);
+        saveScore();
         setTimeout(() => {
             router.back();
         }, 0);
