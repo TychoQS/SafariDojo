356d33a71baa2964c0f492852b67f2c65797e82c
Laura <laurahn07@gmail.com>
2025-05-06 18:18:56 +0100

    fixed Hydration errors

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -4,18 +4,21 @@ import { AuthProvider } from "@/pages/context/AuthContext";
 import "@/styles/globals.css";
 import { I18nextProvider } from 'react-i18next';
 import i18n from '@/i18n';
+import ClientOnly from '@/components/ClientOnly';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <I18nextProvider i18n={i18n}>
-            <AuthProvider>
-                <ProfileProvider>
-                    <GoalsProvider>
-                        <Component {...pageProps} />
-                    </GoalsProvider>
-                </ProfileProvider>
-            </AuthProvider>
-        </I18nextProvider>
+        <AuthProvider>
+            <ProfileProvider>
+                <GoalsProvider>
+                    <ClientOnly>
+                        <I18nextProvider i18n={i18n}>
+                            <Component {...pageProps} />
+                        </I18nextProvider>
+                    </ClientOnly>
+                </GoalsProvider>
+            </ProfileProvider>
+        </AuthProvider>
     );
 }
 
